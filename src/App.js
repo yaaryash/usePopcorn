1a4475849242dd1dc258e547a5dfb89e7a6769b6
yaaryash <yashwalke3434@gmail.com>
2024-04-11 23:14:13 +0530

    new components

--- src/App.js
+++ src/App.js
@@ -147,3 +147,13 @@ function Box({ children }) {
     </div>
   );
 }
+
+function MovieList({ movies, onSelectMovie }) {
+  return (
+    <ul className="list list-movies">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
+      ))}
+    </ul>
+  );
+}
